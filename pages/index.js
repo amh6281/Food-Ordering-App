031f019f4e8120ab02bf6e98f9f908a3ce9060c4
amh6281 <amh6281@naver.com>
2022-08-11 14:34:14 +0900

    add: 이미지 Slice 구현 2022.08.11

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
+import Featured from "../components/Featured";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
@@ -15,7 +16,7 @@ export default function Home() {
           rel="stylesheet"
         />
       </Head>
-      homepage
+      <Featured />
     </div>
   );
 }
