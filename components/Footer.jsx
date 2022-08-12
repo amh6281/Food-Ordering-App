0a92fff3e5099e0bc0c904570e7affca02142c02
amh6281 <amh6281@naver.com>
2022-08-12 12:18:18 +0900

    add: Footer 컴포넌트 추가 2022.08.12
    위치, 영업시간 등 간단한 소개로 구성된 Footer 컴포넌트를 추가한다.

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,54 @@
+import Image from "next/image";
 import React from "react";
+import styles from "../styles/Footer.module.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <Image src="/img/bg.png" objectFit="cover" layout="fill" alt="" />
+      </div>
+      <div className={styles.item}>
+        <div className={styles.card}>
+          <h2 className={styles.motto}>맛 좋은 맛있는 피자집입니다.</h2>
+        </div>
+        <div className={styles.card}>
+          <h1 className={styles.title}>오시는 길</h1>
+          <p className={styles.text}>
+            OO시 OO구 OO동 123번지
+            <br /> OO빌딩 1층
+            <br /> 010-1234-5678
+          </p>
+          <p className={styles.text}>
+            OO시 OO구 OO동 456번지
+            <br /> OO빌딩 3층
+            <br /> 010-1234-5678
+          </p>
+          <p className={styles.text}>
+            OO시 OO구 OO동 789번지
+            <br /> OO빌딩 2층
+            <br /> 010-1234-5678
+          </p>
+          <p className={styles.text}>
+            OO시 OO구 OO동 135번지
+            <br /> OO빌딩 1층
+            <br /> 010-1234-5678
+          </p>
+        </div>
+        <div className={styles.card}>
+          <h1 className={styles.title}>영업시간</h1>
+          <p className={styles.text}>
+            월요일 ~ 금요일
+            <br /> 11:00 – 22:00
+          </p>
+          <p className={styles.text}>
+            토요일 ~ 일요일
+            <br /> 12:00 – 24:00
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
