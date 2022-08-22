b1abcc71121934ca07703cbd071d0ebc1588a49f
amh6281 <amh6281@naver.com>
2022-08-22 13:35:02 +0900

    add: Product 데이터 생성 후 fetch 2022.08.22
    Product에 대한 데이터를 생성한 후, fetch하여 props를 통해 데이터를 전달한다.

--- components/PizzaCard.jsx
+++ components/PizzaCard.jsx
@@ -2,15 +2,13 @@ import Image from "next/image";
 import React from "react";
 import styles from "../styles/PizzaCard.module.css";
 
-const PizzaCard = () => {
+const PizzaCard = ({ pizza }) => {
   return (
     <div className={styles.container}>
-      <Image src="/img/pizza.png" alt="" width="500" height="500" />
-      <h1 className={styles.title}>페퍼로니 피자</h1>
-      <span className={styles.price}>₩12,900</span>
-      <p className={styles.desc}>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel facilis ?
-      </p>
+      <Image src={pizza.img} alt="" width="500" height="500" />
+      <h1 className={styles.title}>{pizza.title}</h1>
+      <span className={styles.price}>₩{pizza.prices[0]}</span>
+      <p className={styles.desc}>{pizza.desc}</p>
     </div>
   );
 };
