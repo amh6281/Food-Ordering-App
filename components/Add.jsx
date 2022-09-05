661bb9cf084b0835fb3db0658fb8309a9b3fc5c9
amh6281 <amh6281@naver.com>
2022-09-05 10:19:25 +0900

    feat: 제품추가 기능 구현 2022.09.05
    관리자인 경우 제품을 추가한다.

--- components/Add.jsx
+++ components/Add.jsx
@@ -1,7 +1,117 @@
-import React from "react";
+import { useState } from "react";
+import styles from "../styles/Add.module.css";
+import axios from "axios";
+import { useRouter } from "next/router";
 
-const Add = () => {
-  return <div>Add</div>;
+const Add = ({ setClose }) => {
+  const [file, setFile] = useState(null);
+  const [title, setTitle] = useState(null);
+  const [desc, setDesc] = useState(null);
+  const [prices, setPrices] = useState([]);
+  const [extra, setExtra] = useState(null);
+  const [extraOptions, setExtraOptions] = useState([]);
+
+  const handleExtraInput = (e) => {
+    setExtra({ ...extra, [e.target.name]: e.target.value });
+  };
+
+  const handleExtra = (e) => {
+    setExtraOptions((prev) => [...prev, extra]);
+  };
+
+  const changePrice = (e, index) => {
+    const currentPrices = prices;
+    currentPrices[index] = e.target.value;
+    setPrices(currentPrices);
+  };
+
+  const handleCreate = async () => {};
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <span onClick={() => setClose(true)} className={styles.close}>
+          X
+        </span>
+        <h1>피자 추가</h1>
+        <div className={styles.item}>
+          <label className={styles.label}>이미지 선택</label>
+          <input type="file" onChange={(e) => setFile(e.target.files[0])} />
+        </div>
+        <div className={styles.item}>
+          <label className={styles.label}>제품명</label>
+          <input
+            className={styles.input}
+            type="text"
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div className={styles.item}>
+          <label className={styles.label}>제품 설명</label>
+          <textarea
+            rows={4}
+            type="text"
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div className={styles.item}>
+          <label className={styles.label}>가격</label>
+          <div className={styles.priceContainer}>
+            <input
+              className={`${styles.input} ${styles.inputSm}`}
+              type="number"
+              placeholder="R"
+              onChange={(e) => changePrice(e, 0)}
+            />
+            <input
+              className={`${styles.input} ${styles.inputSm}`}
+              type="number"
+              placeholder="L"
+              onChange={(e) => changePrice(e, 1)}
+            />
+            <input
+              className={`${styles.input} ${styles.inputSm}`}
+              type="number"
+              placeholder="F"
+              onChange={(e) => changePrice(e, 2)}
+            />
+          </div>
+        </div>
+        <div className={styles.item}>
+          <label className={styles.label}>추가 선택</label>
+          <div className={styles.extra}>
+            <input
+              className={`${styles.input} ${styles.inputSm}`}
+              type="text"
+              placeholder="Item"
+              name="text"
+              onChange={handleExtraInput}
+            />
+            <input
+              className={`${styles.input} ${styles.inputSm}`}
+              type="number"
+              placeholder="Price"
+              name="price"
+              onChange={handleExtraInput}
+            />
+            <button className={styles.extraButton} onClick={handleExtra}>
+              추가하기
+            </button>
+          </div>
+          <div className={styles.extraItems}>
+            {extraOptions.map((option) => (
+              <span key={option.text} className={styles.extraItem}>
+                {option.text}
+              </span>
+            ))}
+          </div>
+        </div>
+        <button className={styles.addButton} onClick={handleCreate}>
+          생성
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Add;
